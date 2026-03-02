aa32cfc58e73e0484e62dd90914d387977f623eb
Lydia-AL <lydia.aitlamara@gmail.com>
2026-03-02 17:59:41 +0100

    Add sys.argv CLI argument support for business date

--- fake_data_app/sensor.py
+++ fake_data_app/sensor.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import date, timedelta
 
 import numpy as np
@@ -46,6 +47,13 @@ class VisitSensor:
         # Return an integer
         return np.floor(visit)
 
+
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        year, month, day = [int(v) for v in sys.argv[1].split("-")]
+    else:
+        year, month, day = 2023, 10, 25
+    queried_date = date(year, month, day)
+
     capteur = VisitSensor(1500, 150)
-    print(capteur.simulate_visit(date(year=2023, month=10, day=25)))
+    print(capteur.simulate_visit(queried_date))
